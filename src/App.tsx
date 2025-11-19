9f1c041e9a481d04a7802c2ad321dd5bf5048dcc
anaxss <henriquesjulya@gmail.com>
2025-11-19 15:49:43 -0300

    update-src

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ThemeProvider } from "./components/ThemeProvider";
 import { Header } from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 import { AboutSection } from "./components/AboutSection";
@@ -21,18 +22,20 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen">
-      <Header onDonateClick={handleDonateClick} />
-      <HeroSection onDonateClick={handleDonateClick} />
-      <AboutSection />
-      <DonationCategories onDonateClick={handleDonateClick} />
-      <DonationSection onDonateClick={handleDonateClick} />
-      <ImpactSection />
-      <Footer onDonateClick={handleDonateClick} />
-      <DonationModal
-        isOpen={isDonationModalOpen}
-        onClose={handleCloseDonationModal}
-      />
-    </div>
+    <ThemeProvider>
+      <div className="min-h-screen">
+        <Header onDonateClick={handleDonateClick} />
+        <HeroSection onDonateClick={handleDonateClick} />
+        <AboutSection />
+        <DonationCategories onDonateClick={handleDonateClick} />
+        <DonationSection onDonateClick={handleDonateClick} />
+        <ImpactSection />
+        <Footer onDonateClick={handleDonateClick} />
+        <DonationModal
+          isOpen={isDonationModalOpen}
+          onClose={handleCloseDonationModal}
+        />
+      </div>
+    </ThemeProvider>
   );
 }
